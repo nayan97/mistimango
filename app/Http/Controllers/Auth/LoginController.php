350a3d10c8bbb7aa5e5e83a21515b9cd9c48c782
nayan97 <nayanislam085@gmail.com>
2023-04-23 11:11:07 +0600

    logout not work

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -36,5 +37,23 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    // custom login
+    public function login(Request $request)
+    {
+        $validated = $request -> validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if (auth() -> attempt(array( 'email' => $request->email, 'password' => $request -> password))) {
+            if (auth()-> user() -> is_admin ==1) {
+                return redirect() -> route('admin.home');
+            } else{
+                return redirect() -> route('home');
+            }
+        }else{
+            return redirect() -> back() -> with('error', 'Invalid email or password'); 
+        }
     }
 }
